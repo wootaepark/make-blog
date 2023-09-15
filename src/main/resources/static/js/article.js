9cda1b85f4f63dcb10b1a6144f3b74eafbdfdea4
wootaepark <pakdal99@gmail.com>
2023-09-15 14:07:24 +0900

    Second_commit_2

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,72 @@
+const deleteButton = document.getElementById('delete-btn');
+
+if(deleteButton){
+    deleteButton.addEventListener('click',event=>{
+
+    let id = document.getElementById('article-id').value;
+    fetch(`/api/articles/${id}`,{
+    method: 'DELETE'
+    })
+    .then(()=>{
+        alert('삭제가 완료되었습니다.');
+        location.replace('/articles');
+
+    })
+    })
+
+}
+
+const modifyButton = document.getElementById('modify-btn');
+
+if(modifyButton){
+
+modifyButton.addEventListener('click',event =>{
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+
+    fetch(`/api/articles/${id}`,{
+        method:'PUT',
+        headers:{
+            "Content-Type" : "application/json",
+        },
+
+        body: JSON.stringify({
+            title:document.getElementById('title').value,
+            content: document.getElementById('content').value,
+
+        })
+    })
+    .then(()=> {
+        alert('수정이 완료되었습니다.');
+        location.replace(`/articles/${id}`);
+    });
+});
+
+}
+
+const createButton = document.getElementById("create-btn");
+if(createButton){
+
+    createButton.addEventListener("click",(event)=>{
+       // const currentTime = new Date();
+        //const formattedTime = `${currentTime.getFullYear()}-${(currentTime.getMonth() + 1).toString().padStart(2, '0')}-${currentTime.getDate().toString().padStart(2, '0')} ${currentTime.getHours().toString().padStart(2, '0')}:${currentTime.getMinutes().toString().padStart(2, '0')}:${currentTime.getSeconds().toString().padStart(2, '0')}`;
+
+
+        fetch("/api/articles",{
+        method:'POST',
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify({
+            title:document.getElementById("title").value,
+            content:document.getElementById("content").value,
+            //createdAt:currentTime,
+        }),
+        }).then(()=>{
+            alert("등록이 완료되었습니다.");
+            location.replace("/articles");
+        })
+    })
+
+
+}
